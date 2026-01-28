ad78848810b800a14c4d4e5609c831ef8ff53e02
Erik Vinicius Pereira <erikvinipereira@gmail.com>
2026-01-27 22:05:18 -0300

    feat: add base template and simple login/logout

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import tickets_home
+from .views import tickets_home, login_view, logout_view
 
 urlpatterns = [
     path('tickets/', tickets_home, name='tickets_home'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def tickets_home(request):
     return render(request, 'tickets/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('tickets_home')
+        messages.error(request, 'Usuário ou senha inválidos.')
+    return render(request, 'auth/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
