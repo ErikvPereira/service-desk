0334e56b85647a96fde2637e1992dfb8294fb26c
Erik Vinicius Pereira <erikvinipereira@gmail.com>
2026-01-29 12:20:44 -0300

    feat: add comments on ticket detail

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Ticket
+from .models import Ticket, TicketComment
 
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ["title", "description", "category", "priority"]
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = TicketComment
+        fields = ['message']
+        
--- tickets/models.py
+++ tickets/models.py
@@ -32,3 +32,17 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f'#{self.id} {self.title}'
+    
+from django.conf import settings
+from django.db import models
+
+
+class TicketComment(models.Model):
+    ticket = models.ForeignKey("Ticket", on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on #{self.ticket_id}"
+
